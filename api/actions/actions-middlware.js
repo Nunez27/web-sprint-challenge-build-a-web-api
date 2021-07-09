1d0d378312d8d5656f34b3167f71054c1521f6ff
Brian Nunez <nunez27@brians-mbp.myfiosgateway.com>
2021-07-09 13:40:18 -0400

    Middlewares completed

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,2 +1,66 @@
 // add middlewares here related to actions
-   
+const Actions = require('../actions/actions-model');
+const Projects = require('../projects/projects-model');
+
+const checkForProjectsId = async (req, res, next) => {
+    const { id } = req.params
+    try {
+        const project = await Projects.get(id)
+        if (!project) {
+            res.status(400).json({
+                message : `No project found with id ${id}`
+            })
+        } else {
+            req.project = project
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error'
+        })
+    }
+}
+
+const checkingForProjectSuccess = (req, res, next) => {
+    if (!req.body.description) {
+        res.status(400).json({message: 'description is needed'})
+    } else {
+        next()
+    }
+}
+
+
+const checkForIdWithActionId = async (req, res, next) => {
+    const { id } = req.params
+    try {
+        const action = await Actions.get(id)
+        if (!action) {
+            res.status(400).json({message : `Project not found with id ${id}`})
+        } else {
+            req.action = action 
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error'
+        })
+    }
+}
+
+const checkIfActionsSucceed = (req, res, next) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({
+            message: 'Name and description are required'
+        })
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    checkIfActionsSucceed,
+    checkForIdWithActionId,
+    checkingForProjectSuccess,
+    checkForProjectsId,
+    
+}
